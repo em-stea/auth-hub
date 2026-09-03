3f2e2465e11244c8b5cd6a728d74f5c38fcdc3c2
Emilia <emilia@androbin.co>
2026-09-03 17:25:10 -0300

    upd

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
+  basePath: "/auth-hub",
   reactCompiler: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
